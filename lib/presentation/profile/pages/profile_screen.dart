74e179b1debd409612e1817b8b06180f2589279d
Sizwe Matenjwa <SizweMatenjwa100@gmail.com>
2025-08-09 08:52:56 +0200

    new features added

--- lib/presentation/profile/pages/profile_screen.dart
+++ lib/presentation/profile/pages/profile_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';git
 import '../../../features/profile/data/profile_repository.dart';
 import '../../../features/profile/logic/profile_bloc.dart';
 import '../../../features/profile/logic/profile_event.dart';
